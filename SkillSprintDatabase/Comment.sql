9bbf385c6032e8f62d62511121404337f02c55a2
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-11 16:29:56 +0100

    Add User_Id and Submission_Id columns to Comment table with foreign key constraints

--- SkillSprintDatabase/Comment.sql
+++ SkillSprintDatabase/Comment.sql
@@ -5,8 +5,10 @@
 	[Title] NVARCHAR (64) NOT NULL,
 	[Content] NVARCHAR (320) NOT NULL,
 	[Created_At] DATETIME2 (7) DEFAULT GETDATE() NOT NULL,
+	[User_Id] UNIQUEIDENTIFIER NOT NULL,  -- !!! column to reference User
+    [Submission_Id] UNIQUEIDENTIFIER NOT NULL, 
 	FOREIGN KEY ([User_Id]) REFERENCES [dbo].[User]([User_Id]) ON DELETE CASCADE,
-	    FOREIGN KEY ([Submission_Id]) REFERENCES [dbo].[Submission]([Submission_Id]) ON DELETE CASCADE 
+	FOREIGN KEY ([Submission_Id]) REFERENCES [dbo].[Submission]([Submission_Id]) ON DELETE CASCADE 
 
 
 )
