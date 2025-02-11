8ce0b288c1f167fb2e2b60d75916f7e77fd549d2
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-11 16:35:15 +0100

    Added foreign key reference for Challenge_Id in Submission table

--- SkillSprintDatabase/Submission.sql
+++ SkillSprintDatabase/Submission.sql
@@ -4,5 +4,8 @@
     [Url] VARCHAR(320) NOT NULL,
     [Description] VARCHAR(320) NOT NULL,
     [User_Id] UNIQUEIDENTIFIER NOT NULL,  
-    FOREIGN KEY ([User_Id]) REFERENCES [dbo].[User]([User_Id]) ON DELETE CASCADE 
+    [Challenge_Id] UNIQUEIDENTIFIER NOT NULL,  
+    FOREIGN KEY ([User_Id]) REFERENCES [dbo].[User]([User_Id]) ON DELETE CASCADE,
+    FOREIGN KEY ([Challenge_Id]) REFERENCES [dbo].[Challenge]([Challenge_Id]) ON DELETE CASCADE 
+
 )
