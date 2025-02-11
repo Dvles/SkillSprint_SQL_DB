51be9836703473fb7eba8609418ae9b07166b123
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-11 16:25:34 +0100

    Created Challenge table

---
+++ SkillSprintDatabase/Challenge.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Challenge](
+
+
+	[Challenge_Id] UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	[Title] NVARCHAR (64) NOT NULL,
+	[Description] NVARCHAR (64) NOT NULL,
+	[StartDate] DATETIME NOT NULL, 
+	[EndDate] DATETIME  NOT NULL,
+	[CATEGORY] NVARCHAR(50) NOT NULL CHECK (Category IN('Design', 'music', 'poetry')) 
+
+
+)
