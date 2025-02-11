b31ad2ac3169ad18ea2e9c136373c9d0f3eea338
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-11 16:28:09 +0100

    Create Comment table

---
+++ SkillSprintDatabase/Comment.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Comment](
+
+
+	[Comment_Id] UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	[Title] NVARCHAR (64) NOT NULL,
+	[Content] NVARCHAR (320) NOT NULL,
+	[Created_At] DATETIME2 (7) DEFAULT GETDATE() NOT NULL,
+	FOREIGN KEY ([User_Id]) REFERENCES [dbo].[User]([User_Id]) ON DELETE CASCADE,
+	    FOREIGN KEY ([Submission_Id]) REFERENCES [dbo].[Submission]([Submission_Id]) ON DELETE CASCADE 
+
+
+)
