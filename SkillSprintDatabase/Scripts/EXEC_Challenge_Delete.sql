9dafd639fdaf3e7359b49748fdc223ecce910767
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-14 16:30:00 +0100

    Successfully tested stored procedures for challenge management (EXEC scripts)

--- SkillSprintDatabase/Scripts/EXEC_Challenge_Delete.sql
+++ SkillSprintDatabase/Scripts/EXEC_Challenge_Delete.sql
@@ -1,2 +1,3 @@
 ﻿EXEC [dbo].[SP_Challenge_Delete]
-    @Challenge_Id = 'your-challenge-id-here';
+    @Challenge_Id = 'cac22a20-4049-44fe-9d90-f7bc8f357637';
+
--- SkillSprintDatabase/Scripts/EXEC_Challenge_Update.sql
+++ SkillSprintDatabase/Scripts/EXEC_Challenge_Update.sql
@@ -1,6 +1,6 @@
 ﻿EXEC [dbo].[SP_Challenge_Update]
-    @Challenge_Id = 'your-challenge-id-here',
-    @Title = 'Updated Challenge Title',
-    @Description = 'Updated description of the challenge.',
-    @StartDate = '2025-02-20',
-    @EndDate = '2025-03-20';
+    @Challenge_Id = '723b65d1-bbc0-42eb-8637-fa1d31f071b4',
+    @Title = 'TESt Updated Challenge Title',
+    @Description = 'Updated description for the challenge. New exciting challenge description.',
+    @StartDate = '2025-05-01',
+    @EndDate = '2025-06-01';
